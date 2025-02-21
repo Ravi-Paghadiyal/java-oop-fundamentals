0d6550600bb428e296ae4560f4aab33b8d7c3b2f
🎓 BCA Student | Year 2 <balrajpaghadiyal@gmail.com>
2025-02-21 22:37:03 +0530

    Arrey_1.java

---
+++ Arrey_1.java
@@ -0,0 +1,10 @@
+public class BasicArray {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};  // Array declaration and initialization
+        
+        System.out.println("Array elements:");
+        for (int num : numbers) {  // Enhanced for loop
+            System.out.print(num + " ");
+        }
+    }
+}
